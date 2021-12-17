945d61d7547b425860e377d348009fcfcfaf776f
Your Name <you@example.com>
2021-12-17 18:55:05 +0530

    methods and inheritance code

---
+++ SingleInheritanceExample/src/MainMethod.java
@@ -0,0 +1,11 @@
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+		Vehicle vehicle = new Vehicle();
+		vehicle.breaks();
+		vehicle.mirror();
+		vehicle.tire();
+		vehicle.engine();
+	}
+}
